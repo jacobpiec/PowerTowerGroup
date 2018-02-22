f739837d59f5a62e65be56be980ccd740f190e5f
Jacob Pieczynski <34606867+JacobPie@users.noreply.github.com>
2018-02-21 18:06:42 -0700

    Delete main.js

--- main.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-  /* Makes the rest of the page appear when see more is clicked */
-  $("#down").on("click", function () {
-    $("#rest").removeClass("invisible");
-  });
-});
